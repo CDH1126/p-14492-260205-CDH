2c29bc745b79fe2bf5a987c4207e6552cc50fc2c
CDH1126 <donghuyn1126@gmail.com>
2026-02-06 10:18:06 +0900

    feat : printStream 및 System.setOut

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,49 @@
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
-public class Main {
-
+public class Main
+{
     public static void main(String[] args) {
-        // "aaa\nbbb\nccc"
-        Scanner sc = new Scanner("""
-                aaa
+//        Scanner scan = new Scanner(System.in);
+        lab2();
+    }
+
+    public static void lab2() {
+
+        PrintStream ORIGINAL_OUT = System.out;
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(byteArrayOutputStream);
+
+        System.setOut(printStream);
+
+        System.out.println("hihi");
+        System.out.println("byebye");
+        System.out.print("ok");
+
+        System.setOut(ORIGINAL_OUT); // 모니터로 출력 복원
+
+        String result = byteArrayOutputStream.toString();
+
+        System.out.println(result);
+    }
+
+    public static void lab1() {
+        Scanner scan = new Scanner("""
+                등록
                 과거에 집착하지 마라.
-                작자 미상
-                ddd
+                작자미상
                 """);
 
-        String str1 = sc.nextLine();
-        String str2 = sc.nextLine();
-        String str3 = sc.nextLine();
-        String str4 = sc.nextLine();
+        String str1 = scan.nextLine();
+        String str2 = scan.nextLine();
+        String str3 = scan.nextLine();
+
 
         System.out.println(str1);
         System.out.println(str2);
         System.out.println(str3);
-        System.out.println(str4);
 
     }
 }
