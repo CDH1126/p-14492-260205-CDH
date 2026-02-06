cac76aa738d434bccfb3f06d10dafe49010b0b79
CDH1126 <donghuyn1126@gmail.com>
2026-02-06 10:31:18 +0900

    feat : 명언게시판 TDD 구현 - Scanner

---
+++ src/test/java/TestUtil.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class TestUtil { // 상태값 존재 X, 객체 생성 X
+
+//    public static String run(String input) {
+//
+//        // 입력값
+//        Scanner scan = new Scanner(input);
+//
+//        // 명언 프로그램 실행
+//
+//        // 출력값
+//    }
+
+    public static Scanner genScanner(String input) {
+        return new Scanner(input);
+    }
+
+}
