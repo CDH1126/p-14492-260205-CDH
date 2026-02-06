645b3b68f82bf921a0c680fdb63a32545a59235f
CDH1126 <donghuyn1126@gmail.com>
2026-02-06 12:18:42 +0900

    feat : 2단계 Green, Blue 구현

--- src/test/java/AppTestRunner.java
+++ src/test/java/AppTestRunner.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 public class AppTestRunner {
 
     public static String run(String input) {
-        Scanner sc = TestUtil.genScanner(input); // Scanner를 얻어와야 함
-
+        Scanner sc = TestUtil.genScanner(input + "\n종료"); // Scanner를 얻어와야 함
+                                // 마지막에 종료 입력값 받아서 자동으로 종료되도록 설정
         ByteArrayOutputStream outputStream = TestUtil.setOutByteArray();
         new App(sc).run();
 
