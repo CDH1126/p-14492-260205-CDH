8181785ae8b02dedb4a44393d4deb2dffad6e6df
CDH1126 <donghuyn1126@gmail.com>
2026-02-06 11:24:26 +0900

    feat : 7 Red 구현 - AppTest

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,3 +1,25 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Scanner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class AppTest {
+
+    @Test
+    @DisplayName("TestUtil.genScanner()")
+    void t1() {
+        Scanner sc = TestUtil.genScanner("종료");
+
+        ByteArrayOutputStream outputStream = TestUtil.setOutByteArray();
+        new App(sc).run();
+
+        String out = outputStream.toString();
+
+        assertThat(out).contains("== 명언 앱 ==");
+
+    }
 
 }
